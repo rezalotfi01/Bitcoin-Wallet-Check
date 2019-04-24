30492c2621263e050418041e34652884af48982d
reza <mr.developerx@gmail.com>
2019-04-24 16:18:08 +0430

    "Observe" method bug fixed

--- app/src/main/java/com/github/rezalotfi01/bwallet/utils/SingleLiveEvent.java
+++ app/src/main/java/com/github/rezalotfi01/bwallet/utils/SingleLiveEvent.java
@@ -23,6 +23,8 @@ import androidx.annotation.MainThread;
 import androidx.annotation.Nullable;
 import android.util.Log;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -42,7 +44,7 @@ public class SingleLiveEvent<T> extends MutableLiveData<T> {
     private final AtomicBoolean mPending = new AtomicBoolean(false);
 
     @MainThread
-    public void observe(LifecycleOwner owner, final Observer<T> observer) {
+    public void observe(@NotNull LifecycleOwner owner, @NotNull final Observer<? super T> observer) {
 
         if (hasActiveObservers()) {
             Log.w(TAG, "Multiple observers registered but only one will be notified of changes.");
